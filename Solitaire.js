106eca779ac5517a5a01ff2f4c5f9c4b3dadf42a
ted <ted@warpedpuppy.com>
2021-10-03 10:53:46 -0400

    cleaned step four a code

--- Solitaire.js
+++ Solitaire.js
@@ -1,9 +1,5 @@
 import VARS from './utils/Vars.js';
-import Deal from './cards/Deal.js';
-import Deck from './cards/Deck.js';
 import Card from './cards/Card.js';
-import MouseDown from './action/MouseDown.js';
-import MouseUp from './action/MouseUp.js';
 import Animate from './action/Animate.js';
 const Solitaire = {
     canvas: undefined,
@@ -21,26 +17,12 @@ const Solitaire = {
     },
     addListeners: function () {
         this.mouseMoveHandler();
-        this.mouseDownHandler();
-        this.mouseUpHandler();
     },
     mouseMoveHandler: function () {
         this.canvas.addEventListener('mousemove', e => {
             let leftOffset = (window.innerWidth - VARS.canvas.width) / 2;
-        
             VARS.mousePoint = {x: e.pageX - leftOffset, y: e.pageY};
-           
          })
-    },
-    mouseDownHandler: function () {
-        this.canvas.addEventListener('mousedown', () => {
-            MouseDown.setActiveCardAndPopulateDragArray();
-        } )
-    },
-    mouseUpHandler: function () {
-        this.canvas.addEventListener('mouseup', e => {
-            if (VARS.activeCard) MouseUp.activeCardExists();
-        });
     }
 }
 export default Solitaire
