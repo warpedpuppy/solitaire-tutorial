31fe4d58cc758a1bf180765a4dd7ac1c48e23cc5
ted <ted@warpedpuppy.com>
2021-10-13 09:46:02 -0400

    comment out shuffle

--- cards/Deck.js
+++ cards/Deck.js
@@ -12,7 +12,7 @@ const Deck = {
                 value ++;
             })
         })
-        VARS.allVisualAssets = this.shuffle(VARS.allVisualAssets)
+        // VARS.allVisualAssets = this.shuffle(VARS.allVisualAssets)
     },
     shuffle: function(arr) {
         let currentIndex = arr.length,  randomIndex;
